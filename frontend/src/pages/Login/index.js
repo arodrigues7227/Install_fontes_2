720f230ce5bf948fcd6cd117481118e72e709f74
arodrigues7227 <arodrigues7227@gmail.com>
2024-09-16 15:54:37 -0300

    Atualizacao 5.3.1
    Atualizacao 5.3.1

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -24,7 +24,7 @@ const useStyles = makeStyles(theme => ({
     },
     leftSide: {
         flex: 1,
-        backgroundImage: "url(https://i.imgur.com/Yu3cnE9.png)",
+        backgroundImage: "url(https://liots.level10tech.com.br/PapelDeParedeInicial.png)",
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         display: 'flex',
