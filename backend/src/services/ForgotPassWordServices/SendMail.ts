fcc16136ad7dbd69964ee277b6ef6ff130914ff2
arodrigues7227 <arodrigues7227@gmail.com>
2024-09-16 17:24:20 -0300

    Atualizacao 5.3

--- backend/src/services/ForgotPassWordServices/SendMail.ts
+++ backend/src/services/ForgotPassWordServices/SendMail.ts
@@ -34,7 +34,7 @@ const SendMail = async (email: string, tokenSenha: string) => {
         const mailOptions = {
           from: fromEmail,
           to: email,
-          subject: "Redefinição de Senha - SWhiteLabel",
+          subject: "Redefinição de Senha - Level10 Tech",
           html: ` <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html dir="ltr" lang="pt">
  <head>
