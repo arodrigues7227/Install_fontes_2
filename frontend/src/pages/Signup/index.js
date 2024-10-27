4056cb8f9669465c9be279e80f58f8e56420ac19
arodrigues7227 <arodrigues7227@gmail.com>
2024-10-27 00:53:31 -0300

    5.4.1

--- frontend/src/pages/Signup/index.js
+++ frontend/src/pages/Signup/index.js
@@ -36,7 +36,7 @@ const Copyright = () => {
 		<Typography variant="body2" color="textSecondary" align="center">
 			{"Copyright © "}
 			<Link color="inherit" href="#">
-				PLW
+				LIOT
 			</Link>{" "}
 		   {new Date().getFullYear()}
 			{"."}
