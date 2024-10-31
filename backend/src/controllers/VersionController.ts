0bc5c883011b5ab07f6026208bc95eb712816cc3
arodrigues7227 <arodrigues7227@gmail.com>
2024-10-31 12:14:38 -0300

    5.4.3

--- backend/src/controllers/VersionController.ts
+++ backend/src/controllers/VersionController.ts
@@ -2,6 +2,6 @@ import { Request, Response } from "express";
 
 export const index = async (req: Request, res: Response): Promise<Response> => {
     return res.status(200).json({
-        version: "v5.4.1"
+        version: "v5.4.3"
     });
 };
